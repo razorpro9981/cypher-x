704882918a67228c8e5d4853221bdb491d8d29a0
razorpro9981 <kissieduebenezer1@gmail.com>
2026-02-20 13:41:04 +0000

    new

--- components/ShuffleText.tsx
+++ components/ShuffleText.tsx
@@ -1,4 +1,7 @@
-
+/* eslint-disable prefer-const */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable react-hooks/set-state-in-effect */
+"use client";
 import React, { useState, useEffect } from 'react';
 
 interface ShuffleTextProps {
